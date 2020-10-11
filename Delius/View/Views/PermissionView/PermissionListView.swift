5822a33f866874ec5c72ef361e18ebeea676a6a1
yoonsoo <dcfdcf456@naver.com>
2020-10-11 19:12:29 +0900

    TAG PermissionVC 꼬밋

--- Delius/View/Views/PermissionView/PermissionListView.swift
+++ Delius/View/Views/PermissionView/PermissionListView.swift
@@ -29,37 +29,51 @@ class PermissionListView:UIButton{
         l.lineBreakMode = .byWordWrapping
         return l
     }()
+    var sub2Label:UILabel={
+        let l = UILabel()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.numberOfLines = 0
+        l.lineBreakMode = .byWordWrapping
+        return l
+    }()
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(img:UIImage,str1:String,str2:String,frame:CGRect) {
+    init(img:UIImage,str1:String,str2:String,str3:String,frame:CGRect) {
         super.init(frame: frame)
         self.translatesAutoresizingMaskIntoConstraints = false
         imgView.image = img
         topLabel.DeliusLabel(txt: str1, txtAlignment: .left, ft: UIFont.DeliusBoldType4(), txtColor: .MainBlackColor())
         subLabel.DeliusLabel(txt: str2, txtAlignment: .left, ft: UIFont.DeliusSystemType1(), txtColor: .MainBlackColor())
-        setView(str1: str1, str2: str2)
+        sub2Label.DeliusLabel(txt: str3, txtAlignment: .left, ft: UIFont.DeliusSystemType1(), txtColor: .MainBlackColor())
+        setView(str1: str1, str2: str2, str3:str3)
     }
-    private func setView(str1:String,str2:String){
+    private func setView(str1:String,str2:String,str3:String){
         self.addSubview(imgView)
-        imgView.leftAnchor.constraint(equalTo: leftAnchor, constant: 40).isActive = true
-        imgView.widthAnchor.constraint(equalToConstant: frame.width/9).isActive = true
-        imgView.heightAnchor.constraint(equalToConstant: frame.width/9).isActive = true
+        imgView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        imgView.widthAnchor.constraint(equalToConstant: frame.width/4).isActive = true
+        imgView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         imgView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
         self.addSubview(topLabel)
         topLabel.topAnchor.constraint(equalTo: imgView.topAnchor, constant: 5).isActive = true
-        topLabel.leftAnchor.constraint(equalTo: imgView.rightAnchor, constant: 10).isActive = true
-        topLabel.rightAnchor.constraint(equalTo: rightAnchor,constant: -20).isActive = true
+        topLabel.leftAnchor.constraint(equalTo: imgView.rightAnchor, constant: 20).isActive = true
         topLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        topLabel.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        self.addSubview(sub2Label)
+        sub2Label.topAnchor.constraint(equalTo: topLabel.topAnchor).isActive = true
+        sub2Label.leftAnchor.constraint(equalTo: topLabel.rightAnchor).isActive = true
+        sub2Label.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        sub2Label.widthAnchor.constraint(equalToConstant: 60).isActive = true
         
+    
         self.addSubview(subLabel)
         subLabel.topAnchor.constraint(equalTo: topLabel.bottomAnchor).isActive = true
         subLabel.bottomAnchor.constraint(equalTo: imgView.bottomAnchor).isActive = true
         subLabel.leftAnchor.constraint(equalTo: topLabel.leftAnchor).isActive = true
-        subLabel.rightAnchor.constraint(equalTo: topLabel.rightAnchor).isActive = true
+        subLabel.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.backgroundColor = UIColor.MainBlackAlpha()
--- Delius/View/Views/PermissionView/PermissionView.swift
+++ Delius/View/Views/PermissionView/PermissionView.swift
@@ -13,7 +13,7 @@ final class PermissionView: UIView{
     
     let topImgView:UIImageView={
         let v = UIImageView()
-        v.image = UIImage(named: "DeliusIcon")
+        v.image = UIImage(named: "DeliusTitle")
         v.translatesAutoresizingMaskIntoConstraints = false
         v.clipsToBounds = true
         return v
@@ -21,7 +21,14 @@ final class PermissionView: UIView{
     
     let titleLabel:UILabel={
         let l = UILabel()
-        l.DeliusLabel(txt: "PermissionView001".localize(), txtAlignment: .center, ft: UIFont.DeliusBoldType4(), txtColor: .MainBlackColor1())
+        l.DeliusLabel(txt: "PermissionView001".toLocal(), txtAlignment: .left, ft: UIFont.DeliusBoldType5(), txtColor: .MainBlackColor())
+        l.numberOfLines = 0
+        l.lineBreakMode = .byWordWrapping
+        return l
+    }()
+    let titleLabel2:UILabel={
+        let l = UILabel()
+        l.DeliusLabel(txt: "PermissionView002".toLocal(), txtAlignment: .left, ft: UIFont.DeliusBoldType5(), txtColor: .MainBlackColor())
         l.numberOfLines = 0
         l.lineBreakMode = .byWordWrapping
         return l
@@ -34,21 +41,9 @@ final class PermissionView: UIView{
         return v
     }()
     
-    let lineView1:UIView={
-        let v = UIView()
-        v.translatesAutoresizingMaskIntoConstraints = false
-        v.backgroundColor = UIColor.MainLightGrayColor()
-        return v
-    }()
-    let lineView2:UIView={
-        let v = UIView()
-        v.translatesAutoresizingMaskIntoConstraints = false
-        v.backgroundColor = UIColor.MainLightGrayColor()
-        return v
-    }()
     let subLabel:UILabel={
         let l = UILabel()
-        l.DeliusLabel(txt: "PermissionView002".localize(), txtAlignment: .left, ft: UIFont.DeliusSystemType2(), txtColor: .MainBlackColor1())
+        l.DeliusLabel(txt: "PermissionView003".toLocal(), txtAlignment: .center, ft: UIFont.DeliusSystemType2(), txtColor: .MainBlackColor1())
         l.numberOfLines = 0
         l.lineBreakMode = .byWordWrapping
         return l
@@ -61,10 +56,9 @@ final class PermissionView: UIView{
     }()
     var gradationView:MainGradientView!
     var nsGradient:NSLayoutConstraint!
-    var startBtn:UIButton={
+    var startBtn:UIButton = {
         let b = UIButton()
-        b.DeliusType1(txt: "PermissionView900".localize(), color: .white, backColor: UIColor.clear)
-        b.titleLabel?.font = UIFont.DeliusBoldType1()
+        b.BtnBlackType(txt: "PermissionView900".toLocal())
         return b
     }()
     
@@ -77,10 +71,9 @@ final class PermissionView: UIView{
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.translatesAutoresizingMaskIntoConstraints = false
-        view1 = PermissionListView(img: UIImage(named: "PermissionLocation")!, str1: "PermissionView100".localize(), str2: "PermissionView101".localize(), frame: CGRect(x: 0, y: 0, width: frame.width, height: 70))
-        view2 = PermissionListView(img: UIImage(named: "PermissionContact")!, str1: "PermissionView200".localize(), str2: "PermissionView201".localize(), frame: CGRect(x: 0, y: 0, width: frame.width, height: 70))
-        view3 = PermissionListView(img: UIImage(named: "PermissionPhoto")!, str1: "PermissionView300".localize(), str2: "PermissionView301".localize(), frame: CGRect(x: 0, y: 0, width: frame.width, height: 70))
-        view4 = PermissionListView(img: UIImage(named: "PermissionPush")!, str1: "PermissionView400".localize(), str2: "PermissionView401".localize(), frame: CGRect(x: 0, y: 0, width: frame.width, height: 70))
+        view1 = PermissionListView(img: UIImage(named: "PermissionLocation")!, str1: "PermissionView100".toLocal(), str2: "PermissionView101".toLocal(), str3:"PermissionView102".toLocal(), frame: CGRect(x: 0, y: 0, width: frame.width, height: 70))
+        view2 = PermissionListView(img: UIImage(named: "PermissionContact")!, str1: "PermissionView200".toLocal(), str2: "PermissionView201".toLocal(),str3:"PermissionView202".toLocal(),  frame: CGRect(x: 0, y: 0, width: frame.width, height: 70))
+        view3 = PermissionListView(img: UIImage(named: "PermissionPhoto")!, str1: "PermissionView300".toLocal(), str2: "PermissionView301".toLocal(),str3:"PermissionView302".toLocal(),  frame: CGRect(x: 0, y: 0, width: frame.width, height: 70))
         gradationView = MainGradientView(frame: CGRect(x:0,y:0,width: frame.width,height:60))
         setConstraints(frame:frame)
         
@@ -91,55 +84,51 @@ final class PermissionView: UIView{
         let heightTerm = frame.height/25
         
         addSubview(topImgView)
-        topImgView.topAnchor.constraint(equalTo: topAnchor, constant: heightTerm).isActive = true
-        topImgView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        topImgView.widthAnchor.constraint(equalToConstant: frame.width/8).isActive = true
-        topImgView.heightAnchor.constraint(equalToConstant: frame.width/8).isActive = true
+        topImgView.topAnchor.constraint(equalTo: topAnchor, constant: heightTerm + 20).isActive = true
+        topImgView.leftAnchor.constraint(equalTo: leftAnchor, constant: 40).isActive = true
+        topImgView.widthAnchor.constraint(equalToConstant:120).isActive = true
+        topImgView.heightAnchor.constraint(equalToConstant: 35).isActive = true
         
-        let str_titleLabel = "PermissionView001".localize()
-        let str_subLabel = "PermissionView002".localize()
+        let str_titleLabel = "PermissionView001".toLocal()
+        let str_subLabel = "PermissionView002".toLocal()
         
         let height_titleLabel = str_titleLabel.StringHeight(withConstrainedWidth: width, font: UIFont.DeliusBoldType4())
         
         let height_subLabel = str_subLabel.StringHeight(withConstrainedWidth: width, font: UIFont.DeliusSystemType2())
         
         addSubview(titleLabel)
-        titleLabel.topAnchor.constraint(equalTo: topImgView.bottomAnchor, constant: heightTerm).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: topImgView.bottomAnchor, constant: 20).isActive = true
         titleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 40).isActive = true
         titleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -40).isActive = true
         titleLabel.heightAnchor.constraint(equalToConstant: height_titleLabel).isActive = true
         
-        addSubview(lineView1)
-        lineView1.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
-        lineView1.leftAnchor.constraint(equalTo: leftAnchor, constant: 40).isActive = true
-        lineView1.rightAnchor.constraint(equalTo: rightAnchor, constant: -40).isActive = true
-        lineView1.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        addSubview(titleLabel2)
+        titleLabel2.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        titleLabel2.leftAnchor.constraint(equalTo: leftAnchor, constant: 40).isActive = true
+        titleLabel2.rightAnchor.constraint(equalTo: rightAnchor, constant: -40).isActive = true
+        titleLabel2.heightAnchor.constraint(equalToConstant: height_titleLabel).isActive = true
+        
         
         self.addSubview(bottomView)
         bottomView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         bottomView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         bottomView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         bottomView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        bottomView.addSubview(gradationView)
-        gradationView.leftAnchor.constraint(equalTo: bottomView.leftAnchor).isActive = true
-        gradationView.topAnchor.constraint(equalTo: bottomView.topAnchor).isActive = true
-        gradationView.widthAnchor.constraint(equalToConstant: frame.width).isActive = true
-        gradationView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        
+      
         addSubview(startBtn)
-        startBtn.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        startBtn.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        startBtn.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        startBtn.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -frame.height/10).isActive = true
+        startBtn.leftAnchor.constraint(equalTo: leftAnchor, constant: 30).isActive = true
+        startBtn.rightAnchor.constraint(equalTo: rightAnchor, constant: -30).isActive = true
         startBtn.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
         addSubview(scrollView)
-        scrollView.topAnchor.constraint(equalTo: lineView1.bottomAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: titleLabel2.bottomAnchor, constant: 90).isActive = true
         scrollView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         scrollView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: startBtn.topAnchor,constant: -20).isActive = true
-        let views = [view1,view2,view3,view4]
+        let views = [view1,view2,view3]
         var number = 1
-        let height : CGFloat = frame.width/5
+        let height : CGFloat = frame.width/4
         for i in views{
             scrollView.addSubview(i!)
             i?.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
@@ -150,16 +139,10 @@ final class PermissionView: UIView{
         }
         scrollView.contentSize = CGSize(width: 0, height: height*4 + height_subLabel)
         
-        scrollView.addSubview(lineView2)
-        lineView2.topAnchor.constraint(equalTo: view4.bottomAnchor,constant: height-frame.width/6).isActive = true
-        lineView2.leftAnchor.constraint(equalTo: scrollView.leftAnchor,constant: 40).isActive = true
-        lineView2.rightAnchor.constraint(equalTo: rightAnchor, constant: -40).isActive = true
-        lineView2.heightAnchor.constraint(equalToConstant: 1).isActive = true
-        
-        scrollView.addSubview(subLabel)
-        subLabel.topAnchor.constraint(equalTo: lineView2.bottomAnchor, constant: 10).isActive = true
+        addSubview(subLabel)
+        subLabel.topAnchor.constraint(equalTo: startBtn.bottomAnchor, constant: 30).isActive = true
         subLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 40).isActive = true
         subLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -40).isActive = true
-        subLabel.heightAnchor.constraint(equalToConstant: height_subLabel).isActive = true
+        subLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 }
